d2db677456268f470e1ad15290f99ea4833f051c
hongnhung <hongnhunglyzin97@gmail.com>
2019-09-30 11:29:12 +0700

    updated tutorials

--- a1/src/App.js
+++ a1/src/App.js
@@ -1,78 +1,16 @@
-import React from 'react';
 import logo from './logo.svg';
-
 import './App.css';
-function One(){
-  return (
-    <div> 
-      <h2> So 1 tap 1</h2>
-      <h3> So 1 tap 2</h3>
-    </div>
-  )
-}
-
-var Two = function(){
-  return (
-    <div>
-      <h2> Cach so 2 </h2>
-    </div>
-  );
-}
-
-var Three = () => (
-                    <div>
-                      <h2> Cach so 3 </h2>
-                    </div>
-                  );
- 
-class Four extends React.Component {
-  render() {
-    return (
-      <div>
-        <button> So 4 </button>
-      </div>
-    );
-  }
-}               
-//  Sử dụng props cach 1
-function NumberOne(props){
-  return(
-    <div>
-      <div className="col-6">
-        <div className="card">
-          <img className="card-img-top" src="http://placehold.it/700x300" alt=""/>
-          <div className="card-body">
-            <h4 className="card-title"> Title </h4>
-            <p className="card-text"> Text </p>
-          </div>
-        </div>
-
-      </div>
-    </div>
-  )
-}
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <One/>
-        <Two/>
-        <Three/>
-        <hr/>
-        <Four/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-
-        </a>
-      </header>
-    </div>
-  );
+import React, {Component} from 'react';
+<script src="anime.min.js"></script>
+class App extends Component {
+    render() {
+        return (
+            < div >
+                demo
+            < /div>
+    )
+        ;
+    }
 }
 
 export default App;
---
+++ animejs/src/App.js
@@ -0,0 +1,97 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import { TransitionGroup, Transition } from 'react-transition-group';
+import anime from 'animejs';
+
+import './style.scss';
+
+class ListItem extends React.Component {
+    constructor() {
+        super();
+        this.liRef = React.createRef();
+    }
+    componentDidUpdate(){
+        this.animeRef = anime({
+            targets: this.liRef.current,
+            translateX: () => {
+                if( this.props.status == 'entering' ) {
+                    return ['-100%', '0%'];
+                } else if( this.props.status == 'exiting' ) {
+                    return ['0%', '100%'];
+                }
+            },
+            elasticity: () => {
+                if( this.props.status == 'entering' ) {
+                    return 300;
+                } else if( this.props.status == 'exiting' ) {
+                    return 0;
+                }
+            },
+            duration: 500
+        });
+    }
+
+    render() {
+        return (
+            <li className="list-item" ref={ this.liRef }>
+            Content of work! <b>{ this.props.num }</b>
+        </li>
+    );
+    }
+}
+
+class App extends Component {
+    constructor() {
+        super();
+
+        this.state = {
+            data: []
+        };
+    }
+    add() {
+        this.setState({
+            ...this.state,
+            data: this.state.data.concat([ this.state.data.length + 1 ])
+        });
+    }
+    remove() {
+        this.setState({
+            ...this.state,
+            data: this.state.data.slice(0, -1)
+        });
+    }
+    render() {
+        return (
+            <div className="app-container">
+                <div className="buttons">
+                <button onClick={ this.add.bind(this) }>Add one</button>
+                <button onClick={ this.remove.bind(this) }>Remove one</button>
+            </div>
+
+        <TransitionGroup
+        component="ul"
+        className="list"
+            >
+            {
+                this.state.data.map( num => (
+                    <Transition
+                key={ num }
+                timeout={ 1000 }
+                mountOnEnter
+                unmountOnExit
+            >
+            {
+            ( status ) => {
+            return <ListItem status={ status } num={ num }/>;
+        }
+    }
+    </Transition>
+    ) )
+    }
+    </TransitionGroup>
+        </div>
+    );
+    }
+}
+
+export default App;
